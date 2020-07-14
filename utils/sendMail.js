b7b378678da7d7278e5a6203d1d4a6bfcc0b8eb0
Alin Rauta <alinrauta@Alins-MacBook-Pro.local>
2020-07-14 13:47:14 +0300

    Changed sendMail to have localhost and real url for links in emails

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -4,6 +4,11 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const sendMail = async (user, emailType) => {
   let msg;
 
+  const url =
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : 'https://app.scrinshot.xyz';
+
   if (emailType === 'email-confirmation') {
     msg = {
       to: user.email,
@@ -11,10 +16,10 @@ const sendMail = async (user, emailType) => {
       subject: 'Please verify your email address',
       html: `<p>Hi ${user.name},</p>
              <p>Welcome to Scrinshot! Please click the link below to complete registration.</p>
-             <p><a href="http://localhost:3000/account/activate/${user.emailVerificationToken}">Activate your account</a></p>
+             <p><a href="${url}/account/activate/${user.emailVerificationToken}">Activate your account</a></p>
              <p>Kind regards,</p>
              <p>The Scrinshot Team</p>
-             <p>Is the link not working? Please copy the following URL in your address bar: <a href="http://localhost:3000/account/activate/${user.emailVerificationToken}">http://localhost:3000/account/activate/${user.emailVerificationToken}</a></p>`,
+             <p>Is the link not working? Please copy the following URL in your address bar: <a href="${url}/account/activate/${user.emailVerificationToken}">${url}/account/activate/${user.emailVerificationToken}</a></p>`,
     };
   } else if (emailType === 'forgot-password') {
     msg = {
@@ -23,11 +28,11 @@ const sendMail = async (user, emailType) => {
       subject: 'Reset Password',
       html: `<p>Hello,</p>
              <p>You are receiving this email because we received a password reset request for your account.</p>
-             <p><a href="http://localhost:3000/auth/forgot-password/${user.passwordResetToken}">Reset password</a></p>
+             <p><a href="${url}/auth/forgot-password/${user.passwordResetToken}">Reset password</a></p>
              <p>If you did not request a password reset, no further action is required.</p>
              <p>Kind regards,</p>
              <p>The Scrinshot Team</p>
-             <p>Is the link not working? Please copy the following URL in your address bar: <a href="http://localhost:3000/auth/forgot-password/${user.passwordResetToken}">http://localhost:3000/auth/forgot-password/${user.passwordResetToken}</a></p>`,
+             <p>Is the link not working? Please copy the following URL in your address bar: <a href="${url}/auth/forgot-password/${user.passwordResetToken}">${url}/auth/forgot-password/${user.passwordResetToken}</a></p>`,
     };
   } else if (emailType === 'email-first-screenshot') {
     msg = {
@@ -37,7 +42,7 @@ const sendMail = async (user, emailType) => {
       html: `<p>Hello,</p>
              <p>You did it!</p>
              <p>You created your first automated screenshot.</p>
-             <p>You can find the first screenshot image in <a href="http://localhost:3000/screenshots/my-screenshots">My Screenshots</a>.
+             <p>You can find the first screenshot image in <a href="${url}/screenshots/my-screenshots">My Screenshots</a>.
              <p>Kind regards,</p>
              <p>The Scrinshot Team</p>`,
     };
@@ -64,7 +69,7 @@ const sendMail = async (user, emailType) => {
       subject: 'You Got A New Screenshot',
       html: `<p>Hello,</p>
              <p>A fresh screenshot has been captured for your screenshot automations</p>
-             <p>You can find it in <a href="http://localhost:3000/screenshots/my-screenshots">My Screenshots</a>.
+             <p>You can find it in <a href="${url}/screenshots/my-screenshots">My Screenshots</a>.
              <p>Kind regards,</p>
              <p>The Scrinshot Team</p>`,
     };
@@ -75,7 +80,7 @@ const sendMail = async (user, emailType) => {
       subject: 'Your screenshot URL is not valid',
       html: `<p>Hello,</p>
              <p>We tried to capture a screenshot to your URL, but unfortunately the URL you entered is not valid.</p>
-             <p>You can go and change it in <a href="http://localhost:3000/screenshots/my-screenshots">My Screenshots</a>.
+             <p>You can go and change it in <a href="${url}/screenshots/my-screenshots">My Screenshots</a>.
              <p>Kind regards,</p>
              <p>The Scrinshot Team</p>`,
     };
